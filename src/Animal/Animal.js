93d8376032e89758bb7c3dc2324955651d3943be
Lindsay Johnston <lindsaykjohnston@outlook.com>
2020-08-08 12:51:30 -0700

    Added animal phootos

--- src/Animal/Animal.js
+++ src/Animal/Animal.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import classes from './Animal.css';
-import dog from './beach-dog.jpeg';
+
 
 const animal = (props) => {
     return (
         <div className={classes.animalCard}>
-            <img className={classes.animalPhoto} src={dog} alt='cute dog'></img>
+            <img className={classes.animalPhoto} src={props.photo} alt='cute animal'></img>
             <div className={classes.petCardBody}>
                 <div className={classes.petCardBodyDetails}>
-                    <h3>Namey-Name</h3>
+                    <h3>{props.name}</h3>
                     <div className={classes.description}>
-                        <p id="animalAge">Puppy</p> 
-                        <li id="breed">Bull dog</li>
+                        <p id="animalAge">{props.age}</p> 
+                        <li id="breed">{props.breed}</li>
                     </div>
-                    <p id="milesAway">3 miles away</p>
+                    <p id="milesAway">{props.milesAway} miles away</p>
                 </div>
             </div>
 
--- src/App.js
+++ src/App.js
@@ -4,25 +4,53 @@ import Navbar from './Navbar/Navbar';
 import ClickHandler from './Navbar/clickHandler';
 import Animal from './Animal/Animal';
 
+//ANIMAL PHOTOS
+import bullDog from './Images-animals/bullDog.jpeg';
+import husky from './Images-animals/husky.jpeg';
+import boyCat from './Images-animals/boyCat.jpg';
+import kitten from './Images-animals/kitten.jpeg';
+import muttPuppy from './Images-animals/muttPuppy.jpg';
 
 class App extends Component {
+  state= {
+    animals: [
+      {id:'adsf', name: 'Sparky', age: 'Puppy', breed: 'Bull dog', milesAway: 3, photo: bullDog },
+      {id:'oerf', name: 'Luna', age: 'Adult', breed: 'Husky', milesAway: 2, photo: husky },
+      {id: '39fj', name: 'Bill', age: 'Kitten', breed: 'Cat', milesAway: 13, photo: kitten},
+      {id:'fjfjf', name: 'Farquat', age: 'Cat', breed: 'Cat', milesAway: 5, photo: boyCat },
+      {id:'049d', name: 'Yummi', age: 'Puppy', breed: 'Mix', milesAway: 3, photo: muttPuppy }
+    ]
+  }
 
+  
 
   render() {
+    let animals = (
+      <div className={classes.main}>
+        {this.state.animals.map((animal, index) => {
+          return <Animal 
+            name={animal.name}
+            age={animal.age}
+            breed={animal.breed}
+            milesAway={animal.milesAway}
+            key={animal.id}
+            photo={animal.photo}
+          />
+        })}
+      </div>
+    );
+
     return (
       <div className="App">
         
           <Navbar
             click={(event) => ClickHandler(event)}
           />
-          <section className={classes.main}>
-            <Animal></Animal>
-            <Animal></Animal>
-            <Animal></Animal>
-            <Animal></Animal>
-            <Animal></Animal>
+        
+            {animals}
+            
 
-          </section>
+
           
 
       </div>

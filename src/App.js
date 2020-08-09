4a4d42b48cae90b7d39df5e59f6e843ff640b10f
Lindsay Johnston <lindsaykjohnston@outlook.com>
2020-08-09 11:14:59 -0700

    Added addFaveAnimalHandler to populate 'Fave' list

--- src/App.js
+++ src/App.js
@@ -7,56 +7,72 @@ import animalsObject from './Animal/Animals-object';
 
 
 class App extends Component {
-  state= {
+  state = {
     animals: animalsObject,
-    showing: 'animals'
+    showing: 'animals',
+    userFaves: []
   }
 
-  deleteAnimalHandler= (index) =>{
-    const newAnimals= [...this.state.animals];
-    newAnimals.splice(index, 1);
-    this.setState({ animals: newAnimals });
+  addFaveAnimalHandler = (id) => {
+    const newAnimals = [...this.state.animals];
+    const newFaves = [...this.state.userFaves];
+    let checkFaves;
+
+    if(newFaves.length >0){
+      checkFaves = newFaves.filter(animal => {
+        return animal.id === id;
+      });
+    };
+    
+    
+    if (checkFaves === undefined) {
+      const fave = newAnimals.find(animal => {
+        return animal.id === id;
+      });
+      newFaves.push(fave);
+      this.setState({ userFaves: newFaves });
+    }
   }
 
-  filterAnimalsHandler=(event) =>{
+  filterAnimalsHandler = (event) => {
     console.log(event.target)
   }
 
-  clickHandler=(event)=>{
+  clickHandler = (event) => {
     console.log(event.target);
-    if(event.target.id==='dogs'){
-        const animals= [...animalsObject];
-        //CHANGE SHOW ANIMALS
-        const showAnimals= animals.filter(animal => animal.type ==='dog');
-        this.setState({ animals: showAnimals, showing: 'dogs' });
-    }
-    if(event.target.id==='cats'){
-      const animals= [...animalsObject];
+    if (event.target.id === 'dogs') {
+      const animals = [...animalsObject];
       //CHANGE SHOW ANIMALS
-      const showAnimals= animals.filter(animal => animal.type ==='cat');
+      const showAnimals = animals.filter(animal => animal.type === 'dog');
+      this.setState({ animals: showAnimals, showing: 'dogs' });
+    }
+    if (event.target.id === 'cats') {
+      const animals = [...animalsObject];
+      //CHANGE SHOW ANIMALS
+      const showAnimals = animals.filter(animal => animal.type === 'cat');
       this.setState({ animals: showAnimals, showing: 'cats' });
     }
-    if(event.target.id==='faves'){
-        this.setState({ showing: 'of your favorite animals' })
-    } if(event.target.id==='search-icon'){
-        alert('search!');
-    } if(event.target.id==='paw'){
+    if (event.target.id === 'faves') {
+      this.setState({ showing: 'of your favorite animals' })
+    } if (event.target.id === 'search-icon') {
+      alert('search!');
+    } if (event.target.id === 'paw') {
       this.setState({ animals: animalsObject, showing: 'animals' });
     }
-};
+  };
 
   render() {
     let animals = (
       <div className={classes.main}>
         {this.state.animals.map((animal, index) => {
-          return <Animal 
+          return <Animal
             name={animal.name}
             age={animal.age}
             breed={animal.breed}
             milesAway={animal.milesAway}
             key={animal.id}
             photo={animal.photo}
-            click={() => this.deleteAnimalHandler(index)}
+            click={() => this.addFaveAnimalHandler(animal.id)}
           />
         })}
       </div>
@@ -64,18 +80,18 @@ class App extends Component {
 
     return (
       <div className="App">
-        
-          <Navbar
-            totalAnimals={this.state.animals.length}
-            click={(event) => this.clickHandler(event)}
-            showing={this.state.showing}
-          />
-        
-          {animals}
-            
+
+        <Navbar
+          totalAnimals={this.state.animals.length}
+          click={(event) => this.clickHandler(event)}
+          showing={this.state.showing}
+        />
+
+        {animals}
 
 
-          
+
+
 
       </div>
     );
